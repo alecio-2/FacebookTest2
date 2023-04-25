90e59a162108ee7d515d586764e36186f196b91a
Alexandru Florin Ciobanu <alecio-2@student.ltu.se>
2023-04-25 10:38:03 +0200

    Update FacebookTestCase1.java

--- src/main/java/FacebookTestCase1.java
+++ src/main/java/FacebookTestCase1.java
@@ -1,16 +1,19 @@
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import java.io.File;
 import java.io.IOException;
 import java.util.List;
-public class FacebookTestCase1 {
+public class FacebookTestCase2 {
     private static final Logger LOGGER = LoggerFactory.getLogger(FacebookTestCase1.class);
 
     public static void main(String[] args) throws InterruptedException {
@@ -81,7 +84,6 @@ public class FacebookTestCase1 {
             String expectedUrl1 = "https://www.facebook.com/";
             String expectedUrl2 = "https://www.facebook.com/?sk=welcome";
             String actualUrl = driver.getCurrentUrl();
-
             if (actualUrl.equals(expectedUrl1) || actualUrl.equals(expectedUrl2)) {
                 LOGGER.debug("Login successful");
             } else {
@@ -90,10 +92,59 @@ public class FacebookTestCase1 {
             Thread.sleep(1000);
 
             // Here starts other tests
+            // Here starts the posting test
 
 
+            // Redirect to the profile page
+            driver.get("https://www.facebook.com/me");
+            LOGGER.info("Redirected to profile page");
+
+
+            //Post from the homepage not from profile page
+            WebElement createPostButton = driver.findElement(By.xpath("//span[contains(text(), \"What's on your mind\")]"));
+            createPostButton.click();
+            LOGGER.debug("Clicked on the first posting field ");
+            Thread.sleep(2000);
+
+            JavascriptExecutor executor = (JavascriptExecutor)driver;
+
+            // Click on the text field
+            WebElement createPostButton2 = driver.findElement(By.xpath("//div[@class='xzsf02u x1a2a7pz x1n2onr6 x14wi4xw x9f619 x1lliihq x5yr21d xh8yej3 notranslate']"));
+            executor.executeScript("arguments[0].click();", createPostButton2);
+            LOGGER.debug("Clicked on the second posting field ");
+            Thread.sleep(2000);
+
+            //Insert the text
+            createPostButton2.sendKeys("This is a test post");
+            LOGGER.debug("Text inserted ");
+            Thread.sleep(2000);
+
+
+            //Click the Post Button v6
+            WebElement postButton = driver.findElement(By.cssSelector("[aria-label='Post']"));
+            postButton.click();
+            LOGGER.debug("Clicked on the Post button");
+
+
+            Thread.sleep(3000);
+            driver.get("https://www.facebook.com/me");
+            LOGGER.info("Redirected to profile page");
+
+
+            // Wait for the post to be visible on the page
+            WebDriverWait wait1 = new WebDriverWait(driver, 3);
+            WebElement postContent = wait1.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//div[contains(text(),'This is a test post')]")));
+
+            String postText = postContent.getText();
+            if (postText.equals("This is a test post")) {
+                LOGGER.debug("Post successful");
+            } else {
+                LOGGER.error("Post failed");
+            }
+
+
+            // Here ends the posting test
             // Here ends other tests
-
 
             //Logging out
             // Click on the profile button and wait for 2 seconds
@@ -104,31 +155,30 @@ public class FacebookTestCase1 {
 
             // Click logout button and verify that user is logged out and redirected to login page
             WebElement logoutButton = driver.findElement(By.xpath("//span[text()='Log Out']"));
-            logoutButton.click();
-            LOGGER.debug("Clicked on the logout button");
-            Thread.sleep(1000);
 
-            // Verify if user is logged out and redirected to login page
-            String loginPageUrl = "https://www.facebook.com/";
-            if (driver.getCurrentUrl().equals(loginPageUrl)) {
-                LOGGER.debug("Logged out successfully");
+            // Check if the logout button is found
+            if(logoutButton.isDisplayed()) {
+                LOGGER.debug("Logout successful."); // Log debug message using logger
             } else {
-                LOGGER.error("Logout failed");
+                LOGGER.error("Logout unsuccessful."); // Log error message using logger
             }
-            Thread.sleep(1000);
 
+            // Click on the logout button
+            logoutButton.click();
+
+            Thread.sleep(1000);
 
         } catch (InterruptedException e) {
             LOGGER.error("InterruptedException occurred: " + e.getMessage());
         } catch (Exception e) {
             LOGGER.error("Exception occurred: " + e.getMessage());
         } finally {
+
             // Close the browser
             driver.quit();
             LOGGER.debug("Browser is closed");
             LOGGER.info("Program is finished");
             LOGGER.info(" ");
-
         }
     }
 }
